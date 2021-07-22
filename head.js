7788d7c3d38e560afae02a44fbc9f2acc3c16f97
Justin Wheeler <justinwwheeler@gmail.com>
2021-07-22 03:52:54 +0000

    first version of this function

---
+++ head.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const head = function(arr) {
+  if (arr.length < 1) {
+    return  undefined;
+  } else {
+    return arr[0];
+  }
+};
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head([1]), 1);
