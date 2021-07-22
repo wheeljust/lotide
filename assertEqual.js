3e6c28b046647815f1870a5f13e8f4b87c8161fd
Justin Wheeler <justinwwheeler@gmail.com>
2021-07-22 03:43:52 +0000

    updated comments

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,5 @@
+/* This solution odoes not work for arrays */
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
