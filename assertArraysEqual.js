a07417cbf641d553b6afe46a19a7156e02e3574e
Justin Wheeler <justinwwheeler@gmail.com>
2021-07-23 20:35:25 +0000

    created new function to use the output of eqArrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const passedMsg = '✅ Assertion Passed';
+const failedMsg = '❌ Assertion Failed';
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  let result = eqArrays(arr1, arr2) ? passedMsg : failedMsg;
+  console.log(result);
+};
+
+//Test Code
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
