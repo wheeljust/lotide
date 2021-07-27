815ff3c286f34163f0e438616316d8e388bb412f
Justin Wheeler <justinwwheeler@gmail.com>
2021-07-27 21:55:16 +0000

    first commit of this function

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, searchValue) {
+  for (const key in obj) {
+    if (obj[key] === searchValue) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// Test Code
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
