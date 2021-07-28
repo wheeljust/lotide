921fa547e7a4d611e870b180168bef820d76a747
Justin Wheeler <justinwwheeler@gmail.com>
2021-07-28 03:55:19 +0000

    first commit of this function

---
+++ assertObjectsEqual.js
@@ -0,0 +1,54 @@
+const passedMsg = '✅ Assertion Passed';
+const failedMsg = '❌ Assertion Failed';
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+
+  for (const key in object1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+const assertObjectsEqual = function(obj1, obj2) {
+  const inspect = require('util').inspect;
+  if (eqObjects(obj1, obj2)) {
+    console.log(`${passedMsg} ${inspect(obj1)} === ${inspect(obj2)}`);
+  } else {
+    console.log(`${failedMsg} ${inspect(obj1)} !== ${inspect(obj2)}`);
+  }
+};
+
+
+// Test Code
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+
+assertObjectsEqual(ab, ba);
+assertObjectsEqual(ab, abc);
