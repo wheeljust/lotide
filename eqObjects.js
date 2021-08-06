7c2e109354299add42d93f2c20a0c79323951251
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-06 04:44:27 +0000

    linted code

--- eqObjects.js
+++ eqObjects.js
@@ -32,7 +32,7 @@ const eqObjects = function(object1, object2) {
       } else if (object1[key] !== object2[key]) return false;
     } else if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) return false;
-    } 
+    }
   }
 
   return true;
