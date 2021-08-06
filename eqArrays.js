0a1396520a1f2b74c9dbc33243fa6e8a36c1b83b
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-06 04:39:27 +0000

    refactored eqArrays to use recursion and now has the ability to compare arguments with nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,18 +7,23 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
+  if (arr1.length !== arr2.length) return false;
+
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) return false;
+    } else if (arr1[i] !== arr2[i]) return false;
   }
   return true;
 };
 
+//Test code for simple arrays
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+//Tests for nested arrays, using recursion
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
