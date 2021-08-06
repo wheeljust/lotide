64e5e722816234e8325c434d159ca1ac58daaef3
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-06 04:26:54 +0000

    refactored code for eqObjects to simplify and  reduce lines

--- eqObjects.js
+++ eqObjects.js
@@ -20,44 +20,41 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-
-// This function does not support comparing objects containing keys that are assigned as objects
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
 
   for (const key in object1) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      console.log('called for eqArrays')
+    if (!(Array.isArray(object1[key]) && Array.isArray(object2[key]))) {
+      if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
+        if (!eqObjects(object1[key], object2[key])) return false;
+      } else if (object1[key] !== object2[key]) return false;
+    } else if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) return false;
-    } else if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
-      console.log('called recursive')
-      if (!eqObjects(object1[key], object2[key])) return false;
-    } else {
-      if (object1[key] !== object2[key]) return false;
-    }
+    } 
   }
+
   return true;
 };
 
 
 // Test Code
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// const abc = { a: "1", b: "2", c: "3" };
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
 
-// assertEqual(eqObjects(ab, ba), true);
-// assertEqual(eqObjects(ab, abc), false);
+assertEqual(eqObjects(ab, ba), true);
+assertEqual(eqObjects(ab, abc), false);
 
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// assertEqual(eqObjects(cd, dc), true);
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
 
-// const cd2 = { c: "1", d: 2 };
-// assertEqual(eqObjects(cd, cd2), false);
+const cd2 = { c: "1", d: 2 };
+assertEqual(eqObjects(cd, cd2), false);
 
-console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
-console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
-console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
-console.log(eqObjects({ a: { z: 1, x: { i: [6, 7] } }, b: 2 }, { a: { z: 1, x: { i: [6, 7] } }, b: 2 })); // => true
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+assertEqual(eqObjects({ a: { z: 1, x: { i: [6, 7] } }, b: 2 }, { a: { z: 1, x: { i: [6, 7] } }, b: 2 }), true);
