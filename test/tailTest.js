ca6d424ea6458985932857a9b66aeea8c34b36ad
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-07 23:41:08 +0000

    updated test to use mocha chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,18 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-//TEST CODE
-assertArraysEqual(tail([1,2,3,4]), [2, 3, 4]);
-assertArraysEqual(tail(['Whoops', 'Light', 'House', 'Labs']), ['Light', 'House', 'Labs']);
+describe('#head tests', () =>  {
+
+  it('returns [2, 3, 4] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+
+  it("returns ['Light', 'House', 'Labs'] for ['Whoops', 'Light', 'House', 'Labs']", () => {
+    assert.deepEqual(tail(['Whoops', 'Light', 'House', 'Labs']), ['Light', 'House', 'Labs']);
+  });
+
+  it('returns [] for []', () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+});
