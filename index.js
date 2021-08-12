a1c2c196fa9acee74f77991988171e01407e8149
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-12 01:15:42 +0000

    updated object to use shorthand sytax to assign functions inside the module exports object

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
 const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 const head = require('./head');
@@ -12,27 +15,23 @@ const letterPositions = require('./letterPositions');
 const countOnly = require('./countOnly');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
-const assertEqual = require('./assertEqual');
-const assertArraysEqual = require('./assertArraysEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
 
 module.exports = {
-  assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  head: head,
-  tail: tail,
-  middle: middle,
-  flatten: flatten,
-  map: map,
-  without: without,
-  takeUntil: takeUntil,
-  countLetters: countLetters,
-  letterPositions: letterPositions,
-  countOnly: countOnly,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-
+  assertEqual,
+  assertArraysEqual,
+  assertObjectsEqual,
+  eqArrays,
+  eqObjects,
+  head,
+  tail,
+  middle,
+  flatten,
+  map,
+  without,
+  takeUntil,
+  countLetters,
+  letterPositions,
+  countOnly,
+  findKey,
+  findKeyByValue,
 };
